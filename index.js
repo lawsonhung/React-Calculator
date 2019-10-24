ef89e2f8a4d41bc6fc6b4fe7727a7b31cc284819
Lawson Hung <lawsonhung@yahoo.com>
2019-10-24 14:48:50 -0400

    Hello World test successful

--- index.js
+++ index.js
@@ -2,8 +2,29 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+/************** Source Code ***********/
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+/************** Source Code ***********/
+
+import React, { Component } from 'react';
+import {
+  Text,
+  AppRegistry
+} from 'react-native';
+
+class ReactCalculator extends Component {
+
+  render() {
+    return (
+      <Text>Hello, React!</Text>
+    )
+  }
+
+}
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
